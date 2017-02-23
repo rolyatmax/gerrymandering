4c2a7a02f77d7fe0cea4bad8b91cdbfe9ed4378b
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-02-23 18:11:24 -0500

    add some data and start working on gerrymandering sketches

--- exploration/index.js
+++ exploration/index.js
@@ -1,51 +1,86 @@
 import {GUI} from 'dat-gui'
-import yo from 'yo-yo'
+import array from 'new-array'
+import Sketch from 'sketch-js'
 
 const container = document.querySelector('.container')
 
 const settings = {
-  canvasSize: 500
+  dotCount: 500,
+  bluePerc: 15
 }
 
-// const gui = new GUI()
-// gui.add(settings, 'canvasSize', 50, 1000).step(10).onChange(renderPage)
+const colors = [
+  [144, 180, 180],
+  [255, 78, 180]
+]
 
-renderPage()
+const gui = new GUI()
+gui.add(settings, 'dotCount', 10, 10000).step(10).onChange(reset)
+gui.add(settings, 'bluePerc', 1, 100).onChange(reset)
+gui.add({ reset }, 'reset')
 
-function renderPage () {
-  const { canvasSize } = settings
-  container.innerHTML = ''
-  const canvas = yo`
-    <canvas
-      height=${canvasSize}
-      width=${canvasSize}
-      style="width: ${canvasSize}px; height: ${canvasSize}px;" />
-  `
-
-  const ctx = canvas.getContext('2d')
-  renderViz(ctx)
-
-  return yo`
-    <div>
-      <h2>gerrymandering exploration</h2>
-      ${canvas}
-    </div>
-  `
+function reset () {
+  sketch.reset()
 }
 
-function renderViz (ctx, screenshots, padding) {
-  const { height, width } = ctx.canvas
-  const center = [width / 2 | 0, height / 2 | 0]
-  const maxMagnitude = (Math.min(height, width) / 2 | 0)
-}
+const sketch = Sketch.create({
+  container: container,
+  setup () {
+    const { height, width } = this.canvas
+    this.center = [width / 2 | 0, height / 2 | 0]
+    this.reset()
+  },
 
-function drawArc (ctx, center, radius, startRad, endRad, width, color) {
-  ctx.strokeStyle = color
-  ctx.lineWidth = width
-  ctx.beginPath()
-  ctx.arc(center[0], center[1], radius, startRad, endRad)
-  ctx.stroke()
-}
+  reset () {
+    this.curLine = []
+    this.boundaries = []
+    this.createDots()
+  },
+
+  draw () {
+    this.dots.forEach(({position, color}) => drawCircle(this, position, 2, color))
+    this.boundaries.forEach((line, i) => {
+      const color = colors[i % colors.length]
+      fillShape(this, line, `rgba(${color.join(', ')}, 0.2)`)
+    })
+    if (this.dragging && this.curLine.length) {
+      drawLine(this, this.curLine, '#333')
+    }
+  },
+
+  mousedown () {
+    this.curLine = []
+  },
+
+  mouseup () {
+    if (this.curLine.length > 1) {
+      const line = this.curLine.concat([this.curLine[0]])
+      this.boundaries.push(line)
+    }
+    this.curLine = []
+  },
+
+  mousemove () {
+    if (!this.dragging) return
+    const lastPt = this.curLine[this.curLine.length - 1]
+    if (lastPt && this.mouse.x === lastPt[0] && this.mouse.y === lastPt[1]) return
+    this.curLine.push([this.mouse.x, this.mouse.y])
+  },
+
+  createDots () {
+    const { height, width } = this.canvas
+    const diameter = Math.min(height, width)
+    const maxMagnitude = (diameter / 2 - diameter * 0.05) | 0
+    this.dots = array(settings.dotCount).map(() => {
+      const rads = Math.random() * Math.PI * 2
+      const mag = Math.random() * maxMagnitude
+      return {
+        position: [Math.cos(rads) * mag + this.center[0], Math.sin(rads) * mag + this.center[1]],
+        color: Math.random() * 100 < settings.bluePerc ? '#00f' : '#f00'
+      }
+    })
+  }
+})
 
 function drawCircle (ctx, position, radius, color) {
   ctx.fillStyle = color
@@ -54,11 +89,22 @@ function drawCircle (ctx, position, radius, color) {
   ctx.fill()
 }
 
-function drawLine (ctx, start, end, color) {
+function fillShape (ctx, points, color) {
+  const start = points[0]
+  ctx.fillStyle = color
+  ctx.lineWidth = 0
+  ctx.beginPath()
+  ctx.moveTo(start[0], start[1])
+  points.slice(1).forEach(pt => ctx.lineTo(pt[0], pt[1]))
+  ctx.fill()
+}
+
+function drawLine (ctx, points, color) {
+  const start = points[0]
   ctx.strokeStyle = color
   ctx.lineWidth = 1
   ctx.beginPath()
   ctx.moveTo(start[0], start[1])
-  ctx.lineTo(end[0], end[1])
+  points.slice(1).forEach(pt => ctx.lineTo(pt[0], pt[1]))
   ctx.stroke()
 }
