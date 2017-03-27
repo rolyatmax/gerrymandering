512529af82f98e5af4844fcc8d8212e0eff7433a
Taylor Baldwin <taylorbaldwin@gmail.com>
2017-03-27 10:11:52 -0400

    updating build, removing data symlink, no more gzip

--- exploration-svg/build.sh
+++ exploration-svg/build.sh
@@ -1,11 +1,8 @@
 rm -rf ../build
 mkdir -p ../build
 node_modules/.bin/browserify index.js > ../build/bundle.js
-gzip -9 ../build/bundle.js
 cp index.css ../build/
 cp reset.css ../build/
 cp index.html ../build/
-cp state-config.js ../build/
-cp helpers.js ../build/
-ln -s ../data ../build/data
+cp -r data ../build/
 cp .now.json ../build/package.json
